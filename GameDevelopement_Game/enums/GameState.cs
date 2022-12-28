3d75d661e3923b24dfafe57d9f7511ed5799c563
Arne <arnev.c.23@gmail.com>
2022-12-28 11:05:27 +0100

    gamestates

---
+++ GameDevelopement_Game/enums/GameState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopement_Game.enums
+{
+    public abstract class GameState
+    {
+        enum CurrentGameState
+        {
+            main_menu,
+            level_1,
+            level_2,
+            gameover
+        }
+    }
+}
